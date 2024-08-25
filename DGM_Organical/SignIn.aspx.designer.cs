4919e5316466c7c62d23324d53adfcf99285be6c
YuMo <79789595+CHGary823@users.noreply.github.com>
2024-08-25 23:25:56 +0800

    sign in page

--- DGM_Organical/SignIn.aspx.designer.cs
+++ DGM_Organical/SignIn.aspx.designer.cs
@@ -13,5 +13,14 @@ namespace DGM_Organical
 
     public partial class LogIn
     {
+
+        /// <summary>
+        /// Login1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Login Login1;
     }
 }
